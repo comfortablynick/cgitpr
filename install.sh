f7ccfee13a263c30f7f3835c45974f51110a6e4f
Nick Murphy <comfortablynick@gmail.com>
2019-03-11 14:55:13 -0500

    Add `conan install` to install script

--- install.sh
+++ install.sh
@@ -4,11 +4,14 @@
 rm -rf build
 mkdir build && cd build || exit 1
 
+# conan install
+conan install ..
+
 # build
 cmake ..
-make
+make install
 
-# link compile_commands.json for YouCompleteMe
+# link compile_commands.json for source code completion
 if [ -f compile_commands.json ]; then
     cd .. || exit 1
     if [ ! -e compile_commands.json ]; then
